286828bd95eee0fcac8f8bc0428dcb7000e9c826
jorge luis yucra <jorgeyh7@gmail.com>
2023-07-07 12:34:50 -0500

    inicio de proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { TimelineModule } from 'primeng/timeline';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    RippleModule,
+    TimelineModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
